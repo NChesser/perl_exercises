ac8a73c49a95cdbcc00708ec98327fab2a03bbf8
Nick Chesser <nick.chesser@outlook.com>
2017-06-23 10:24:09 +0930

    Add files via upload

--- next_prime.pl
+++ next_prime.pl
@@ -12,30 +12,27 @@ print "Enter number \n";
 my $total = <STDIN>;
 
 sub getPrimes{
-        my $t = shift;
+	my $t = shift;
 
-        if($t > 0){
-
-			while($cnt <= $t){
-				$cnt++;
-				$prime = 1;
-					for(my $i=2; $i<$cnt; $i++){
-						if($cnt%$i == 0){
-							$prime = 0;
-						}
-
+	if($t > 0){
+		while($cnt <= $t){
+			$cnt++;
+			$prime = 1;
+				for(my $i=2; $i<$cnt; $i++){
+					if($cnt%$i == 0){
+						$prime = 0;
 					}
-
-				if($prime == 1){
-					print $cnt;
-					print "\n";
 				}
-			}		
-		}else{
-			print "Invalid input: Try again \n";
-				my $try = <STDIN>;
-					getPrimes($try);
-		}
+			if($prime == 1){
+				print $cnt;
+				print "\n";
+			}
+		}		
+	}else{
+		print "Invalid input: Try again \n";
+			my $try = <STDIN>;
+				getPrimes($try);
+	}
 }
 
 getPrimes($total);
