910bfd74d0f72fa88bd3f44382e9a20476eb4d9c
Nick Chesser <nick.chesser@outlook.com>
2017-06-29 14:21:10 +0930

    Add files via upload

---
+++ collatz.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Scalar::Util qw(looks_like_number);
+
+print "Enter your number \n";
+
+my $num = <STDIN>;
+my $cnt = 0;
+
+if(looks_like_number($num)){
+        while($num > 1){
+
+                if($num%2 == 0){
+
+                        $num = $num/2;
+
+                }else{
+                        $num = $num*3+1;
+                }
+                $cnt++;
+        }
+
+        print "Number of steps = $cnt \n";
+}else{
+        print "No number entered \n";
+}
