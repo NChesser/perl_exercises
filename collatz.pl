6cd452fb4a12ed788668ae4251b4b85dc1be3a25
Nick Chesser <nick.chesser@outlook.com>
2017-06-29 14:23:59 +0930

    Add files via upload

--- collatz.pl
+++ collatz.pl
@@ -11,19 +11,15 @@ my $num = <STDIN>;
 my $cnt = 0;
 
 if(looks_like_number($num)){
-        while($num > 1){
-
-                if($num%2 == 0){
-
-                        $num = $num/2;
-
-                }else{
-                        $num = $num*3+1;
-                }
-                $cnt++;
-        }
-
-        print "Number of steps = $cnt \n";
+    while($num > 1){
+            if($num%2 == 0){
+                    $num = $num/2;
+            }else{
+                    $num = $num*3+1;
+            }
+            $cnt++;
+    }
+    print "Number of steps = $cnt \n";
 }else{
-        print "No number entered \n";
+    print "No number entered \n";
 }
